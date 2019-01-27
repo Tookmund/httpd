c7ad12d48363d89f28b9f9f7bab12257f3e3dceb
Jacob Adams <tookmund@gmail.com>
2019-01-27 13:56:31 -0500

    Log Requests

--- main.go
+++ main.go
@@ -12,7 +12,10 @@ func main() {
 		port = os.Args[1]
 	}
 	fs := http.FileServer(http.Dir("."))
-	http.Handle("/", fs)
+	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+		log.Printf("%s %s %s\n",req.Method, req.URL, req.Proto)
+		fs.ServeHTTP(rw, req)
+	})
 	log.Printf("Listening on %s...\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
