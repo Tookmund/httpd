f5831a806dd12ef2c78aa4613c52033ad4de68ac
Jacob Adams <tookmund@gmail.com>
2019-01-27 13:51:15 -0500

    Add optional argument to change address and port to listen on

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	port := ":8080"
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+	}
 	fs := http.FileServer(http.Dir("."))
 	http.Handle("/", fs)
 	log.Printf("Listening on %s...\n", port)
