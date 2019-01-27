089b216d7d28d1272b46ffa75b559e0f15f60e4e
Jacob Adams <tookmund@gmail.com>
2019-01-27 13:59:40 -0500

    Rename port addr as that's more accurate

--- main.go
+++ main.go
@@ -7,15 +7,15 @@ import (
 )
 
 func main() {
-	port := ":8080"
+	addr := ":8080"
 	if len(os.Args) > 1 {
-		port = os.Args[1]
+		addr = os.Args[1]
 	}
 	fs := http.FileServer(http.Dir("."))
 	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
 		log.Printf("%s %s %s\n",req.Method, req.URL, req.Proto)
 		fs.ServeHTTP(rw, req)
 	})
-	log.Printf("Listening on %s...\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	log.Printf("Listening on %s...\n", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
