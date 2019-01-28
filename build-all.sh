e096a41a5c5a93756f6d182c5f777daf4d241f4c
Jacob Adams <tookmund@gmail.com>
2019-01-28 11:40:22 -0500

    Put binaries in separate build directory

--- build-all.sh
+++ build-all.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+mkdir -p build
 
 for o in windows linux darwin freebsd
 do
 	for a in 386 amd64 arm arm64
 	do
 		echo "$o $a"
-		GOOS="$o" GOARCH="$a" go build -o "httpd-$o-$a"
+		GOOS="$o" GOARCH="$a" go build -o "build/httpd-$o-$a"
 	done
 done
